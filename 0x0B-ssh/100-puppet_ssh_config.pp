39a1585f729a135579ec17ce2a4bcb5e728eafe0
Mitchkal <mitchellkalenda@gmail.com>
2023-11-28 05:22:19 -0500

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -6,7 +6,7 @@ file { '/etc/ssh/ssh_config':
   group   => 'ubuntu',
   mode    => '0600',
   content => '
-    Host 18.206.197.179
+    Host 54.158.208.173
         IdentityFile ~/.ssh/school
         PreferredAuthentications publickey
         PasswordAuthentication no
