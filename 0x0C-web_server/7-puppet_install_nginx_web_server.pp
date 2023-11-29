bb2ce662bb01612c82d94e0d75267eaaa4f05808
Mitchkal <mitchellkalenda@gmail.com>
2023-11-28 19:20:00 -0500

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -12,15 +12,18 @@ file_line{ 'aaaaa':
   ensure => present,
   path   => 'etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGU1wu4 permanent;',
+  line   => '    rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGU1wu4 permanent;',
+  notify => Service['nginx'],
 }
 
 # root block
 file { '/var/www/html/index.html':
+  ensure   => file
   content  => 'Hello World!',
 }
 
 service { 'nginx':
   ensure  => running,
+  enable  => true,
   require => Package['nginx'],
 }
