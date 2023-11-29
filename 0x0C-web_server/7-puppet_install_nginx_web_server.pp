3c3b0377bda2713584dd9def85ff86ba16d467fa
Mitchkal <mitchellkalenda@gmail.com>
2023-11-28 19:13:38 -0500

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -6,36 +6,21 @@ package { 'nginx':
   ensure => installed,
 }
 
-# configure
-class { 'nginx':
-  listen_port => '80',
-}
 
 # defaullt server
-nginx::resource::vhost { 'default':
-  ensure  => present,
-  listen  => ['80 default_server'],
-  options => {
-    'rewrite' => '^/redirect_me https://www.youtube.com/watch?v=QH2-TGU1wu4 permanent',
-  }'
+file_line{ 'aaaaa':
+  ensure => present,
+  path   => 'etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGU1wu4 permanent;',
 }
 
 # root block
-nginx::resource::location { 'root':
-  ensure   => present,
-  vhost    => 'default',
-  location => '/',
+file { '/var/www/html/index.html':
   content  => 'Hello World!',
 }
 
-# 404 page
-nginx:: resource::location { 'error_page_404':
-  ensure   => present
-  vhost    => 'default'
-  location => '/page_error_404.html',
-  options  => {
-    'internal' => true,
-  },
-  content => 'Ceci n\'est pas une page',
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
-
