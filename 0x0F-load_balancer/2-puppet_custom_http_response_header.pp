08a6c748fbca8c12cd707616d258d112b6170a6c
Mitchkal <mitchellkalenda@gmail.com>
2023-12-04 17:38:41 -0500

    2-puppet_custom_http_response_header.pp

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,9 @@
+#Automate custom server http header creation
+
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HostName;" /etc/nginx/sites-available/default;
+  service ngix restart',
+  provider => shell;
+}
