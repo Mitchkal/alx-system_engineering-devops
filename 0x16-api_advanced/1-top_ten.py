7fe6341a41fcba7812bd7bf126f535f7fd7fd9e1
Mitchkal <mitchellkalenda@gmail.com>
2024-01-16 18:53:36 -0500

    top ten posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""module top ten"""
+import requests
+
+
+def top_ten(subreddit):
+    """querries Reddit API abd returns top ten posts"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+
+    headers = {
+        'User-Agent': 'My User Agent 5.0'}
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        data = response.json()['data']
+        top = (data['children'])
+        for item in top[:10]:
+            print(item['data']['title'])
+    except Exception as e:
+        return (None)
