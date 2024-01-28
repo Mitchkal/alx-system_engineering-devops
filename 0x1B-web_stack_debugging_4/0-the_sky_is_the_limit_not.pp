8ecccfd6628a5fdfb5f324c82eb67686a5e79373
Mitchkal <mitchellkalenda@gmail.com>
2024-01-28 12:30:24 -0500

    fix nginx open file limit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# Fix nginx too many open files
+
+exec {'modify maximum open file limit':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx && sudo service nginx restart',
+  path    => 'usr/local/bin',
+}
